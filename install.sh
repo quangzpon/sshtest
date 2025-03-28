abccc08afaac097d4c34eedfdcc4f6bd3c58ca8e
quangzpon <kiemdaidaukien@gmail.com>
2025-03-28 21:51:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,45 +1,32 @@
-#!/bin/bash
-
-set -e  # Dừng ngay nếu có lỗi
-
+#!/bin/sh
 random() {
-    tr </dev/urandom -dc A-Za-z0-9 | head -c5
-    echo
+	tr </dev/urandom -dc A-Za-z0-9 | head -c5
+	echo
 }
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 gen64() {
-    ip64() {
-        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-    }
-    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+	ip64() {
+		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+	}
+	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
-
-install_packages() {
-    echo "Detecting OS and installing required packages..."
-    if [[ -f /etc/debian_version ]]; then
-        apt update && apt install -y gcc make net-tools curl unzip iptables
-    elif [[ -f /etc/redhat-release ]]; then
-        yum install -y gcc make net-tools curl unzip iptables
-    else
-        echo "Unsupported OS. Please use Ubuntu, Debian, CentOS, or RHEL."
-        exit 1
-    fi
-}
-
 install_3proxy() {
-    echo "Installing 3proxy..."
-    wget -qO- https://github.com/z3APA3A/3proxy/archive/refs/tags/0.9.4.tar.gz | tar -xz
-    cd 3proxy-0.9.4
+    echo "installing 3proxy"
+    URL="https://raw.githubusercontent.com/quayvlog/quayvlog/main/3proxy-3proxy-0.8.6.tar.gz"
+    wget -qO- $URL | bsdtar -xvf-
+    cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
-    cd ..
-    rm -rf 3proxy-0.9.4
+    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    chmod +x /etc/init.d/3proxy
+    chkconfig 3proxy on
+    cd $WORKDIR
 }
 
-gen_3proxy_config() {
-    cat <<EOF >/usr/local/etc/3proxy/3proxy.cfg
+gen_3proxy() {
+    cat <<EOF
 daemon
 maxconn 1000
 nscache 65536
@@ -49,7 +36,7 @@ setuid 65535
 flush
 auth strong
 
-users $(awk -F "/" '{print $1 ":CL:" $2 " "}' ${WORKDATA})
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 
 $(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
@@ -58,41 +45,10 @@ $(awk -F "/" '{print "auth strong\n" \
 EOF
 }
 
-create_systemd_service() {
-    cat <<EOF >/etc/systemd/system/3proxy.service
-[Unit]
-Description=3proxy Proxy Server
-After=network.target
-
-[Service]
-ExecStart=/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
-Restart=always
-User=nobody
-LimitNOFILE=65535
-
-[Install]
-WantedBy=multi-user.target
+gen_proxy_file_for_user() {
+    cat >proxy.txt <<EOF
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
-    systemctl enable 3proxy
-    systemctl start 3proxy
-}
-
-gen_data() {
-    seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
-    done
-}
-
-gen_iptables() {
-    awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 " -m state --state NEW -j ACCEPT"}' ${WORKDATA} >/etc/iptables.rules
-    iptables-restore < /etc/iptables.rules
-    iptables-save >/etc/iptables.conf
-}
-
-gen_ip6addr() {
-    awk -F "/" '{print "ip -6 addr add " $5 "/64 dev eth0"}' ${WORKDATA} >/etc/netplan/ipv6-add.sh
-    chmod +x /etc/netplan/ipv6-add.sh
-    bash /etc/netplan/ipv6-add.sh
 }
 
 upload_proxy() {
@@ -104,34 +60,62 @@ upload_proxy() {
     echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
     echo "Download zip archive from: ${URL}"
     echo "Password: ${PASS}"
+
+}
+gen_data() {
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
+    done
 }
 
-### Main script execution ###
-echo "Installing dependencies..."
-install_packages
+gen_iptables() {
+    cat <<EOF
+    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+EOF
+}
 
+gen_ifconfig() {
+    cat <<EOF
+$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
+EOF
+}
+echo "installing apps"
+yum -y install gcc net-tools bsdtar zip >/dev/null
+
+install_3proxy
+
+echo "working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
-mkdir -p $WORKDIR && cd $_
+mkdir $WORKDIR && cd $_
 
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "Detected IPv4 = ${IP4}, IPv6 Subnet = ${IP6}"
-echo "Enter the number of proxies to create (e.g., 500):"
+echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
+
+echo "How many proxy do you want to create? Example 500"
 read COUNT
 
 FIRST_PORT=10000
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
-echo "Generating proxy data..."
 gen_data >$WORKDIR/data.txt
-gen_iptables
-gen_ip6addr
-gen_3proxy_config
-create_systemd_service
+gen_iptables >$WORKDIR/boot_iptables.sh
+gen_ifconfig >$WORKDIR/boot_ifconfig.sh
+chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
 
-echo "Generating proxy list for users..."
-awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA} >proxy.txt
+gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
+
+cat >>/etc/rc.local <<EOF
+bash ${WORKDIR}/boot_iptables.sh
+bash ${WORKDIR}/boot_ifconfig.sh
+ulimit -n 10048
+service 3proxy start
+EOF
+
+bash /etc/rc.local
+
+gen_proxy_file_for_user
 
 upload_proxy
